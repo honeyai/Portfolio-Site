1381fc18accefdc10876c9e7dfe84263cdbf7cf8
Jessica <contact.jessicasanchez@gmail.com>
2020-09-13 12:16:27 -0400

    attempting to animate page

---
+++ src/Components/ColorCascade.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../Styles/animations.css";
+import "../Styles/colorCascade.css";
+
+const ColorCascade = () => {
+  return (
+    <div className="color__CascadeWrapper">
+      <div className="animation__topDiv" ></div>
+      <div className="animation__midDiv" ></div>
+      <div className="animation__bottomDiv" ></div>
+    </div>
+  );
+};
+
+export default ColorCascade;
