db7a5ca6aafaeefa70d662876051f24241476595
Jessica <contact.jessicasanchez@gmail.com>
2020-09-13 14:19:57 -0400

    beginning the hero

---
+++ src/Components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../Styles/hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero__Image">
+      <p>
+      I am the hero with text
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
