e480af28bcd922f687d20c8cba7ade1af1582bdf
Jessica <contact.jessicasanchez@gmail.com>
2020-09-18 18:24:23 -0400

    leaving aesthetic moving on to links

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -3,27 +3,31 @@ import "../Styles/intro.css";
 import myPic from "../Assets/Images/myPic.jpg"; // Tell webpack this JS file uses this image
 
 const Intro = () => {
-  console.log(myPic);
   return (
     <div className="intro__wrapper">
-      <div className="intro__image">
-        <img alt="Jessica" src={myPic} />
-      </div>
-      <p className="intro__text">
-        <span className="intro__span">
-          Hey there!
-          <span role="img" aria-label="Waving hand emoji">
-            👋
+      <span id="pls">
+        <div className="intro__image">
+          <img alt="Jessica" src={myPic} />
+        </div>
+        <p className="intro__text intro__body">
+          <span className="intro__span">
+            Hey there!
+            <span role="img" aria-label="Waving hand emoji">
+              👋
+            </span>
           </span>
-        </span>
-        <span className="intro__body">
-          My name is Jessica Sanchez. 
-        </span>
-        <span className="intro__body">
-          I'm a full stack developer, an artist, and
-          a polyglot.
-        </span>  
-      </p>
+          <br></br>
+          <span className="intro__body">
+            My name is Jessica Sanchez. 
+          </span>
+          <br></br>
+          <span className="intro__body"> 
+            I'm a full stack developer, an artist, and
+            a polyglot.
+
+          </span>  
+        </p>
+      </span>
       
     </div>
   );
