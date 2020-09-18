9e0c07807662dbc77e5f73228cad25497ba28800
Jessica <contact.jessicasanchez@gmail.com>
2020-09-18 14:01:30 -0400

    attempting wrap around my picture

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import '../Styles/intro.css';
-import myPic from '../Assets/Images/myPic.jpg'; // Tell webpack this JS file uses this image
+import React from "react";
+import "../Styles/intro.css";
+import myPic from "../Assets/Images/myPic.jpg"; // Tell webpack this JS file uses this image
 
 const Intro = () => {
   console.log(myPic);
   return (
     <div className="intro__wrapper">
-      <p className="intro__text">
-        Hey there! <span role="img" aria-label="Waving hand emoji"> 👋 </span> <br/>
-        My name is Jessica Sanchez. 
-        <br/>
-        I'm a full stack developer, an artist, and a polyglot.
-      </p>
       <div className="intro__image">
         <img alt="Jessica" src={myPic} />
       </div>
+      <p className="intro__text">
+        <span className="intro__span">
+          Hey there!
+          <span role="img" aria-label="Waving hand emoji">
+            👋
+          </span>
+        </span>
+        <span className="intro__body">
+          My name is Jessica Sanchez. 
+        </span>
+        <span className="intro__body">
+          I'm a full stack developer, an artist, and
+          a polyglot.
+        </span>  
+      </p>
+      
     </div>
   );
 };
