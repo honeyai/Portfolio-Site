d43f4642aae5f6cb0336f1bc76bcaabd0b8f9cd0
Jessica <contact.jessicasanchez@gmail.com>
2020-09-23 02:19:08 -0400

    drafting the links and ids for the sections to come

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,24 +1,22 @@
 import React, { useState } from "react";
 import "../Styles/intro.css";
 import myPic from "../Assets/Images/myPic.jpg"; // Tell webpack this JS file uses this image
-import { useScrollPosition } from '@n8tb1t/use-scroll-position';
+import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 
 const Intro = () => {
   //i want my animation to trigger th second that intro__wrapper is right at the top of the screen
 
   const [startAnimation, setAnimate] = useState(false);
 
-
-
   useScrollPosition(({ prevPos, currPos }) => {
     console.log(currPos.x);
     console.log(currPos.y);
 
-    if(currPos.y <= -400) {
+    if (currPos.y <= -400) {
       console.log("this is when the emoji should animate");
       setAnimate(true);
     }
-  })
+  });
 
   return (
     <div className="intro__wrapper">
@@ -29,14 +27,23 @@ const Intro = () => {
         <p className="intro__text intro__body">
           <span className="intro__span">
             Hey there!
-            <div role="img" aria-label="Waving hand emoji" className={startAnimation ? "intro__emojiAnimate" : null}>
-              👋
+            <div className={startAnimation ? "intro__emojiAnimate" : null}>
+              <span role="img" aria-label="Waving hand emoji">
+                👋
+              </span>
             </div>
           </span>
           <span className="intro__body">My name is Jessica Sanchez.</span>
           <br></br>
           <span className="intro__body">
-            I'm a full stack developer, an artist, and a polyglot.
+            <a
+              href="https://www.instagram.com/owlturdeth/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              artist
+            </a>
+            , and a <a href="#resume__languages"> polyglot </a>.
           </span>
         </p>
       </span>
