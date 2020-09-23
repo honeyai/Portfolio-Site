0ed457dd7b8b2bc89ef8f5ad8374062144816ff5
Jessica <contact.jessicasanchez@gmail.com>
2020-09-23 02:09:42 -0400

    will refine the rotate of the emoji

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/intro.css";
 import myPic from "../Assets/Images/myPic.jpg"; // Tell webpack this JS file uses this image
+import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 
 const Intro = () => {
   //i want my animation to trigger th second that intro__wrapper is right at the top of the screen
 
-//if i can do the equivalence of
-//let scrollTop = window.scrollY
-//let introWrapper = document.querySelector('intro__wrapper')
+  const [startAnimation, setAnimate] = useState(false);
 
-//target === srcElement (latter is for compatibility)
 
-//event.target.introWrapper.scrollTop >= a number i have to find that number.
+
+  useScrollPosition(({ prevPos, currPos }) => {
+    console.log(currPos.x);
+    console.log(currPos.y);
+
+    if(currPos.y <= -400) {
+      console.log("this is when the emoji should animate");
+      setAnimate(true);
+    }
+  })
 
   return (
     <div className="intro__wrapper">
@@ -22,11 +29,10 @@ const Intro = () => {
         <p className="intro__text intro__body">
           <span className="intro__span">
             Hey there!
-            <span role="img" aria-label="Waving hand emoji">
+            <div role="img" aria-label="Waving hand emoji" className={startAnimation ? "intro__emojiAnimate" : null}>
               👋
-            </span>
+            </div>
           </span>
-          <br></br>
           <span className="intro__body">My name is Jessica Sanchez.</span>
           <br></br>
           <span className="intro__body">
